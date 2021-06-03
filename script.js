f2f690a34a959dddb0010d6dd6db96a863a2171c
Demid <bydimko@yandex.ru>
2021-06-04 02:42:44 +0300

    lesson1

---
+++ script.js
@@ -0,0 +1,14 @@
+const tempCel = 15
+const tempFar = ((9 / 5) * tempCel + 32)
+alert(tempFar)
+
+let admin
+let name
+name = "Василий"
+admin = name
+alert(admin)
+
+var i = 1000
+var c = "108"
+alert(i + c)
+// 1000 + "108" = 1000108
